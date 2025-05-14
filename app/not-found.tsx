b81789c1cba2cbf1f967c3f48b4eef74a571d4ab
Declan <199662174+declanhuggins@users.noreply.github.com>
2025-05-14 02:52:11 -0500

    update package.json with dependency overrides and improved tailwindcss utilization

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,58 +1,27 @@
 export const runtime = "edge";
+import type { Metadata } from "next";
+
+import Head from "next/head";
+import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <>
-      <title>404: This page could not be found.</title>
-      <div style={styles.error}>
-        <div>
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
-            }}
-          />
-          <h1 className="next-error-h1" style={styles.h1}>
-            404
-          </h1>
-          <div style={styles.desc}>
-            <h2 style={styles.h2}>This page could not be found.</h2>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen font-mono p-8 sm:p-20 uppercase">
+      <Head>
+        <title>404: Page Not Found</title>
+      </Head>
+      <h1 className="text-4xl font-bold text-center">
+        Oops, this page cannot be found.
+      </h1>
+      <Link href="/" className="btn-fill mt-4 font-semibold">
+          <span className="mr-2">→</span>
+          AFROTC Home
+      </Link>
+    </div>
   );
 }
 
-const styles = {
-  error: {
-    fontFamily:
-      'system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
-    height: "100vh",
-    textAlign: "center",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  desc: {
-    display: "inline-block",
-  },
-
-  h1: {
-    display: "inline-block",
-    margin: "0 20px 0 0",
-    padding: "0 23px 0 0",
-    fontSize: 24,
-    fontWeight: 500,
-    verticalAlign: "top",
-    lineHeight: "49px",
-  },
-
-  h2: {
-    fontSize: 14,
-    fontWeight: 400,
-    lineHeight: "49px",
-    margin: 0,
-  },
-} as const;
+export const metadata: Metadata = {
+  title: "404: Page Not Found",
+  description: "Oops, this page cannot be found",
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-mono)] p-8 sm:p-20">
+    <div className="flex flex-col items-center justify-center min-h-screen font-mono p-8 sm:p-20">
       <h1 className="text-4xl font-bold text-center">
-        This is NOT{" "}
-        <a
-          className="text-blue-500 underline"
+        This is NOT <Link
           href="https://www.afrotc.com"
           target="_blank"
           rel="noopener noreferrer"
+          className="text-blue-500 underline"
         >
           afrotc.com
-        </a>
+        </Link>
       </h1>
-      <p className="mt-4 text-center text-xl">Hello det 225 :)</p>
+      <p className="mt-4 text-center text-xl">
+        Hello det 225 :)
+      </p>
     </div>
   );
 }
